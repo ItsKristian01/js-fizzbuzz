094f1dd70821d608fd5e971de1a723478d31df82
ItsKristian01 <kristianhana11@gmail.com>
2023-10-20 16:03:21 +0200

    update: for

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
+const ulElem = document.querySelector("li");
+
 // Program Logic
+let numberList = "";
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
-} 
+    let result;
+    if (i % 3 === 0 && i % 5 === 0) {
+        result = "FizzBuzz";
+    } else if (i % 3 === 0) {
+        result = "Fizz";
+    } else if (i % 5 === 0) {
+        result = "Buzz";
+    }
+    console.log(` ${i} ${result}`);
+}
+
+
+
+
 
 // Output
